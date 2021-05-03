91da5c87812c5932a2edde46faabfdb69c2e7860
naisthorpe <aisthorpe@gmail.com>
2021-05-03 19:02:35 -0400

    Change profile from modal to template

---
+++ public/js/map-marker.js
@@ -0,0 +1,11 @@
+const element = document.getElementById("map-markers");
+
+const numberOfGeocaches = element.getElementsByTagName("*").length;
+
+console.log(element.children)
+
+/*
+for (let i=1; i<numberOfGeocaches+1; i++) {
+    let geocache[i] = element.children.
+}
+*/
