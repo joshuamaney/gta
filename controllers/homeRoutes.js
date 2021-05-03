39fbf8739283969ce4c134eee342296a5f0174e6
naisthorpe <aisthorpe@gmail.com>
2021-05-02 21:40:14 -0400

    Can't get handlebars to pull data from database

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
+    // Get all geocaches and join with user data
     const geocacheData = await Geocache.findAll({
       include: [
         {
@@ -13,9 +14,8 @@ router.get('/', async (req, res) => {
       ]
     });
 
-    const geocaches = geocacheData.map((geocache) => geocache.get({plain:true}));
-    // Pass serialized data and session flag into template
-    console.log(geocaches[0]);
+    let geocaches = geocacheData.map((geocache) => geocache.get({plain:true}));
+    
     res.render('map', {
       geocaches,
       logged_in: req.session.logged_in
@@ -25,25 +25,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/post/:id', async (req, res) => {
+router.get('/geocache/:id', withAuth, async (req, res) => {
   try {
-    const postData = await Post.findByPk(req.params.id, {
+    const geocacheData = await Geocache.findByPk(req.params.id, {
       include: [
         {
           model: User,
           attributes: {exclude: ["password"]}
-        },
-        {
-          model: Comment,
-          attributes: ["id", "comment_text", "post_id", "user_id", "date_created"]
         }
       ],
     });
 
-    const post = postData.get({ plain: true });
-
-    res.render('post-by-id', {
-      ...post,
+    let geocaches = geocacheData.map((geocache) => geocache.get({plain:true}));
+    
+    res.render('map', {
+      geocaches,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -52,19 +48,23 @@ router.get('/post/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/user', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Post }, { model: Comment }],
+      include: [
+        {
+          model: Geocache
+        }
+      ],
     });
 
-    const user = userData.get({ plain: true });
+    const users = userData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
+
+    res.render('map', {
+      users,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -79,26 +79,6 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }, { model: Comment }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('dashboard', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 router.get("/signup", (req, res) => {
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,21 @@
-// Edit profile
+const renderProfile = async (event) => {
+    event.preventDefault();
 
-// 
+    const response = await fetch('/user', {
+        method: 'GET',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        console.log("sick!");
+        // If successful, do nothing!
+        return;
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document
+    .querySelector('#profile-btn')
+    .addEventListener('click', renderProfile);
