01e73c26da4c2439a5dd061019686db6b7d37975
naisthorpe <aisthorpe@gmail.com>
2021-05-07 18:45:36 -0400

    Delete specific geocache added

--- controllers/api/geocacheRoutes.js
+++ controllers/api/geocacheRoutes.js
@@ -15,7 +15,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete('/delete/:id', withAuth, async (req, res) => {
   try {
     const geocacheData = await Geocache.destroy({
       where: {
--- public/js/geocache.js
+++ public/js/geocache.js
@@ -3,3 +3,27 @@
 // Edit geocache
 
 // Delete geocache
+
+const deleteGeocache = async (event) => {
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+
+    const element = event.target;
+
+    console.log("hit");
+    console.log(element.getAttribute("data-index"));
+
+    let id = element.getAttribute("data-index");
+
+    const response = await fetch(`/api/geocaches/delete/${id}`, {
+        method: "DELETE",
+    });
+
+    if (response.ok) {
+        document.location.replace('/profile');
+    } else {
+        alert("Failed to delete geocache")
+    }
+};
+
+$(document).on("click", '.geocache-delete-btn', deleteGeocache);
